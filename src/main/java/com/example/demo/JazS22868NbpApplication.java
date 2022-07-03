e5f478a00fd24c6fe7e6be95a70eb95547f41973
Mateusz Budzyński <s22868@pjwstk.edu.pl>
2022-07-03 14:20:27 +0200

    init with RestTemplate config and db conn in app properties

---
+++ src/main/java/com/example/demo/JazS22868NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS22868NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS22868NbpApplication.class, args);
+	}
+
+}
