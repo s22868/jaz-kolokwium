6c0c3f12f9ceb3f4c688126e062854af5c7a4b08
Mateusz Budzyński <s22868@pjwstk.edu.pl>
2022-07-03 14:26:53 +0200

    add nbpresponse & rate models

---
+++ src/main/java/com/example/demo/models/NBPResponse.java
@@ -0,0 +1,11 @@
+package com.example.demo.models;
+
+import java.util.List;
+
+public class NBPResponse {
+    private String table; //: "A",
+    private String no; //: "1/A/NBP/2012",
+    private String effectiveDate; //: "2012-01-02",
+    private List<Rate> rates;
+
+}
---
+++ src/main/java/com/example/demo/models/Rate.java
@@ -0,0 +1,7 @@
+package com.example.demo.models;
+
+public class Rate {
+    private String currency; //: "bat (Tajlandia)",
+    private String code; //: "THB",
+    private Double mid; // 0.1095
+}
