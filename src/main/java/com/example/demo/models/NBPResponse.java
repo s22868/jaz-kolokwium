7b86693fdbdf4cef365074393187eef38653876c
Mateusz Budzyński <s22868@pjwstk.edu.pl>
2022-07-03 14:27:35 +0200

    add getters, setters and constructors

--- src/main/java/com/example/demo/models/NBPResponse.java
+++ src/main/java/com/example/demo/models/NBPResponse.java
@@ -8,4 +8,42 @@ public class NBPResponse {
     private String effectiveDate; //: "2012-01-02",
     private List<Rate> rates;
 
+    public NBPResponse(String table, String no, String effectiveDate, List<Rate> rates) {
+        this.table = table;
+        this.no = no;
+        this.effectiveDate = effectiveDate;
+        this.rates = rates;
+    }
+
+    public String getTable() {
+        return table;
+    }
+
+    public void setTable(String table) {
+        this.table = table;
+    }
+
+    public String getNo() {
+        return no;
+    }
+
+    public void setNo(String no) {
+        this.no = no;
+    }
+
+    public String getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(String effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    public List<Rate> getRates() {
+        return rates;
+    }
+
+    public void setRates(List<Rate> rates) {
+        this.rates = rates;
+    }
 }
--- src/main/java/com/example/demo/models/Rate.java
+++ src/main/java/com/example/demo/models/Rate.java
@@ -4,4 +4,34 @@ public class Rate {
     private String currency; //: "bat (Tajlandia)",
     private String code; //: "THB",
     private Double mid; // 0.1095
+
+    public Rate(String currency, String code, Double mid) {
+        this.currency = currency;
+        this.code = code;
+        this.mid = mid;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Double getMid() {
+        return mid;
+    }
+
+    public void setMid(Double mid) {
+        this.mid = mid;
+    }
 }
